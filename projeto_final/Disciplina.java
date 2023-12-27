568cbb64dbba133e0fa1051a26ffe602787c2e00
Bendelack <rickk1010@gmail.com>
2023-12-27 07:27:18 -0300

    Projeto, atualizacao nas classes Disciplina e Professor.

--- projeto_final/Disciplina.java
+++ projeto_final/Disciplina.java
@@ -1,7 +1,33 @@
 package projeto_final;
 
+import java.util.ArrayList;
+
 public class Disciplina {
     private String nome;
     private Professor professor;
-    private Aluno[] alunos;
+    private ArrayList<Aluno> alunos;
+
+    public Disciplina ( String nome, String professor ) {
+        this.nome = nome;
+        this.professor = new Professor(professor, this.nome);
+        this.alunos = new ArrayList<>();
+    }
+
+    public String getNome ( ) {
+        return this.nome;
+    }
+
+    public void setNome ( String nome ) {
+        this.nome = nome;
+    }
+
+    //metodo para inserir aluno na disciplina
+    public void inserirAluno (Aluno aluno){
+        this.alunos.add(aluno);
+    };
+
+    //metodo para remover aluno da disciplina
+    public void removerAluno (Aluno aluno){
+        this.alunos.remove(aluno);
+    };
 }
--- projeto_final/Professor.java
+++ projeto_final/Professor.java
@@ -2,5 +2,11 @@ package projeto_final;
 
 public class Professor {
     private String nome;
+    private String disciplina;
+
+    public Professor ( String nome, String disciplina ) {
+        this.nome = nome;
+        this.disciplina = disciplina;
+    }
     
 }
